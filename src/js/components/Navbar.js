0e8bf70acec67711faff782efe8df219db43c4b8
Will Mann <william@mannclan.net>
2018-05-28 13:33:57 -0500

    Fixed Navbar styling.

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -4,11 +4,18 @@ import { NavLink } from 'react-router-dom';
 import logo from './../../assets/images/Logo Icon White.png';
 class Navbar extends React.Component {
   render() {
+      const houseID = this.props.user.user.house;
+      const houseName = this.props.houses.houses.filter((house) => {
+          return house._id === houseID;
+      }).length > 0 ? " - " + this.props.houses.houses.filter((house) => {
+          return house._id === houseID;
+      })[0].name: "";
     return (
       <nav className="main-topnav">
         <img
           src={logo}
           style={{ float: 'left', margin: 5, width: 36, height: 36 }}
+          alt="RIDGE"
         />
         <NavLink to="/">Home</NavLink>
         <NavLink to="/students">Students</NavLink>
@@ -20,13 +27,14 @@ class Navbar extends React.Component {
         {this.props.user.user.role <= 1 ? (
           <NavLink to="/houses">Houses</NavLink>
         ) : null}
+        <a style={{float: 'right'}}>{this.props.user.user.firstname + " " + this.props.user.user.surname + houseName}</a>
       </nav>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { user: state.user };
+  return { user: state.user, houses: state.houses };
 }
 
 export default connect(mapStateToProps)(Navbar);
